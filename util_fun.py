636c4b82f9ae23adff82b7841aaa0a331f21c5cc
Ashvin Oli <40764714+ashvinoli@users.noreply.github.com>
2022-01-18 15:28:46 +0545

    COrrection to sorting again

--- util_fun.py
+++ util_fun.py
@@ -23,15 +23,24 @@ class util_functions:
     @classmethod
     def sort_them(cls,list_of_episodes):
         all_episodes = []
+        all_numbers = True
         for _ in list_of_episodes:
             episode_name = _
             last_piece = os.path.split(episode_name)[-1]
+
+            #Look for numbers in the episode's name if found use them to sort else just use the default i.e string name
             episode_number = re.findall("[0-9]+",last_piece)
-            actual_number = int("".join(episode_number))
-            temp = episode(episode_name,actual_number)
-            all_episodes.append(temp)
-        #all_episodes.sort(key = lambda x:x.episode_name)
-        all_episodes.sort(key = lambda x:x.episode_number)
+            if len(episode_number)>0:
+                actual_number = int("".join(episode_number))
+                temp = episode(episode_name,actual_number)
+                all_episodes.append(temp)
+                all_numbers = True
+            else:
+                all_numbers = False
+        if not all_numbers:
+            all_episodes.sort(key = lambda x:x.episode_name)
+        else:
+            all_episodes.sort(key = lambda x:x.episode_number)
         return [x.episode_name for x in all_episodes]
         
 
